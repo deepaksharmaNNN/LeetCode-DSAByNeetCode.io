752d5ccfc3633a7330d8d2317e118769a02c054e
Deepak Sharma <dsharma2828@gmail.com>
2024-12-05 12:41:58 +0530

    1688. Count of Matches in Tournament

--- Math_and_Geometry/Easy/1688_Count_of_Matches_in_Tournament.java
+++ Math_and_Geometry/Easy/1688_Count_of_Matches_in_Tournament.java
@@ -3,3 +3,20 @@ class Solution {
         return n - 1;
     }
 }
+/*
+class Solution {
+    public int numberOfMatches(int n) {
+        int num = n;
+        int res = 0;
+        while(num > 1){
+            res += num / 2;
+            if(num % 2 == 1){
+                num = num / 2 + 1;
+            }else{
+                num = num / 2;
+            }
+        }
+        return res;
+    }
+}
+*/
