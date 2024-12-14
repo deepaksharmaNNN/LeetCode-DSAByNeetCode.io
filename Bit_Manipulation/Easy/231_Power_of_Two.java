634b808e1989dddf5a58ca3e87968025e12265d6
Deepak Sharma <dsharma2828@gmail.com>
2024-12-14 18:31:51 +0530

    231. Power of Two

--- Bit_Manipulation/Easy/231_Power_of_Two.java
+++ Bit_Manipulation/Easy/231_Power_of_Two.java
@@ -5,3 +5,16 @@ class Solution {
         return (n & (n - 1)) == 0;
     }
 }
+/*
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        if(n < 0) return false;
+        while(n > 0){
+            if(n == 1) return true;
+            if(n % 2 != 0) return false;
+            n /= 2;
+        }
+        return false;
+    }
+}
+ */
